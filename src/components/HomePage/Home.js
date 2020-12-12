3a2909b92d09ba38ac00f3a82fe08e6008256ef5
rawanmsallam <msallamrawan0@gmail.com>
2020-12-13 01:20:55 +0200

    edit a data file

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import BodySection from "../Body/BodySection"
-import {homeObjOne} from "./Data"
+import {homeObjOne,homeObjTwo,homeObjThree,homeObjFour} from "./Data"
 
 function Home() {
     return (
         <>
             <BodySection {...homeObjOne}/>
+            <BodySection {...homeObjTwo}/>
+            <BodySection {...homeObjThree}/>
+            <BodySection {...homeObjFour}/>
         </>
     );
 }
