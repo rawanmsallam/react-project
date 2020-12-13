68177d5daedcc61689c4ed0ffa4941d506ffbe59
rawanmsallam <msallamrawan0@gmail.com>
2020-12-13 03:01:11 +0200

    add Pricing files

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import BodySection from "../Body/BodySection"
+import Pricing from '../Pricing/Pricing'
 import {homeObjOne,homeObjTwo,homeObjThree,homeObjFour} from "./Data"
 
 function Home() {
@@ -8,7 +9,9 @@ function Home() {
             <BodySection {...homeObjOne}/>
             <BodySection {...homeObjTwo}/>
             <BodySection {...homeObjThree}/>
+            <Pricing/>
             <BodySection {...homeObjFour}/>
+           
         </>
     );
 }
